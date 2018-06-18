540d5e03177938d7d802583b7e17d3f54687cc07
Chad Carbert <chad.carbert@kloeckner.com>
2018-06-18 16:52:04 +0200

    Add inbetween redirect after login via index route beforeModel

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,5 +2,7 @@ import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Route.extend(ApplicationRouteMixin, {
-  routeAfterAuthentication: 'after-login'
+  // `routeAfterAuthentication` defaults to 'login'
+  // via ApplicationRouteMixin
+  // routeAfterAuthentication: 'after-login'
 });
